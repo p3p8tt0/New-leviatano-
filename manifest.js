c81aeaaf6a064b70f0b89b19a94c775d98057e63
p3p8tt0 <p3p8slt1@proton.me>
2026-06-11 21:48:58 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,36 +1,15 @@
-// Fix per Node 18 (undici)
-if (typeof File === "undefined") {
-  globalThis.File = class File extends Blob {
-    constructor(chunks, filename, options = {}) {
-      super(chunks, options);
-      this.name = filename;
-      this.lastModified = options.lastModified || Date.now();
-    }
-  };
+{
+  "id": "vavooitalia",
+  "name": "Vavoo Italia",
+  "description": "🇮🇹 Canali TV italiani in diretta da Vavoo.to. Solo canali con paese = Italia.",
+  "version": "1.0.0",
+  "author": "TuoNome",
+  "logo": "https://vavoo.tv/favicon.ico",
+  "supportedTypes": ["tv"],
+  "filename": "providers/vavooitalia.js",
+  "enabled": true,
+  "contentLanguage": ["it"],
+  "formats": ["m3u8"],
+  "limited": false,
+  "supportsExternalPlayer": true
 }
-
-const express = require("express");
-const app = express();
-
-// Carica il core Leviatano
-try {
-  require("./db-helper.js");
-  require("./leviathan-pack-resolver.js");
-  require("./formatter.js");
-  require("./ranking.js");
-  require("./smart_parser.js");
-  require("./p2p_handler.js");
-  console.log("🦑 Leviathan core caricato.");
-} catch (err) {
-  console.error("❌ Errore nel core:", err);
-}
-
-app.get("/", (req, res) => {
-  res.send("🦑 Leviatano attivo su Hugging Face!");
-});
-
-const PORT = process.env.PORT || 7000;
-
-app.listen(PORT, () => {
-  console.log("🚀 Leviathan attivo su porta " + PORT);
-});
